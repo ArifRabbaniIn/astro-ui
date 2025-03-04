ade05bac561ede16947921c7b95195c8992d94ad
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-04 19:10:02 +0530

    package: add radio input component

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,4 +1,5 @@
 import Input from './components/Input.astro';
 import NumberInput from './components/NumberInput.astro';
+import RadioInput from './components/RadioInput.astro';
 
-export { Input, NumberInput };
+export { Input, NumberInput, RadioInput };
