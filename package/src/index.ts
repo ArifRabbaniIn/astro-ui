cf8c504c49d321f81002e0096eb2c31f00873e95
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-04 22:02:56 +0530

    package: export newly created components from entry file

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,19 @@
+import Accordion from './components/Accordion.astro';
+import AccordionContent from './components/AccordionContent.astro';
+import AccordionItem from './components/AccordionItem.astro';
+import AccordionTrigger from './components/AccordionTrigger.astro';
 import Input from './components/Input.astro';
+import JoinedContainer from './components/JoinedContainer.astro';
 import NumberInput from './components/NumberInput.astro';
 import RadioInput from './components/RadioInput.astro';
 
-export { Input, NumberInput, RadioInput };
+export {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Input,
+  JoinedContainer,
+  NumberInput,
+  RadioInput,
+};
