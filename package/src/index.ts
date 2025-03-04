f77007599e6d5aa8e2cb8ab420fae873cede8eb9
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-04 22:11:59 +0530

    package: add JoinedItem component

--- package/src/index.ts
+++ package/src/index.ts
@@ -4,6 +4,7 @@ import AccordionItem from './components/AccordionItem.astro';
 import AccordionTrigger from './components/AccordionTrigger.astro';
 import Input from './components/Input.astro';
 import JoinedContainer from './components/JoinedContainer.astro';
+import JoinedItem from './components/JoinedItem.astro';
 import NumberInput from './components/NumberInput.astro';
 import RadioInput from './components/RadioInput.astro';
 
@@ -14,6 +15,7 @@ export {
   AccordionTrigger,
   Input,
   JoinedContainer,
+  JoinedItem,
   NumberInput,
   RadioInput,
 };
