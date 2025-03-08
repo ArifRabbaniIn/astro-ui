a9f9af892baea2ec276f19b4982b905d93138ed0
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-08 18:19:56 +0530

    package: encapsulate 404 block variation

---
+++ package/src/blocks/404/types.ts
@@ -0,0 +1,8 @@
+import type { ComponentProps } from 'astro/types';
+import type NotFound1 from './NotFound1.astro';
+
+export type NotFoundProps = NotFound1;
+
+type NotFound1 = {
+  variation: '1';
+} & ComponentProps<typeof NotFound1>;
