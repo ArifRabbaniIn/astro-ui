cdd5cf283bc4bb26c3246f1bd5c5f311b948feb2
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-08 15:34:05 +0530

    package: add util for bg and content color for focus state

--- package/src/utils/bgAndContentColor.ts
+++ package/src/utils/bgAndContentColor.ts
@@ -17,3 +17,21 @@ export function getBgAndContentColorClass(color: Color) {
 
   return classObject[color];
 }
+
+export function getBgAndContentColorClassForFocus(color: Color) {
+  const classObject: Record<Color, string> = {
+    primary: 'focus:bg-primary focus:bg-primary-content',
+    secondary: 'focus:bg-secondary focus:bg-secondary-content',
+    success: 'focus:bg-success focus:bg-success-content',
+    warning: 'focus:bg-warning focus:bg-warning-content',
+    neutral: 'focus:bg-neutral focus:bg-neutral-content',
+    info: 'focus:bg-info focus:bg-info-content',
+    error: 'focus:bg-error focus:bg-error-content',
+    accent: 'focus:bg-accent focus:bg-accent-content',
+    'base-100': 'focus:bg-base-100 focus:bg-base-content',
+    'base-200': 'focus:bg-base-200 focus:bg-base-content',
+    'base-300': 'focus:bg-base-300 focus:bg-base-content',
+  };
+
+  return classObject[color];
+}
