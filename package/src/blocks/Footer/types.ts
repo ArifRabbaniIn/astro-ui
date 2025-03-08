b492ed4393009125c68a4ebd5a3cb3baf3a9a119
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-08 18:15:58 +0530

    package: fix

--- package/src/blocks/Footer/types.ts
+++ package/src/blocks/Footer/types.ts
@@ -1,8 +1,8 @@
 import type { ComponentProps } from 'astro/types';
 import type Footer1 from './Footer1.astro';
 
-export type FooterProps = Footer1Type;
+export type FooterProps = Footer1;
 
-type Footer1Type = {
+type Footer1 = {
   footerStyle: '1';
 } & ComponentProps<typeof Footer1>;
