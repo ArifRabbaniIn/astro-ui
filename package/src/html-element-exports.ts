00b63d35d03c6b97fa6e8429110a18b5d13a6b20
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-12 19:39:09 +0530

    add footer html element

--- package/src/html-element-exports.ts
+++ package/src/html-element-exports.ts
@@ -1,4 +1,5 @@
 import Body from './html-element/Body.astro';
 import Main from './html-element/Main.astro';
+import Footer from './html-element/Footer.astro';
 
-export { Body, Main };
+export { Body, Main, Footer as FooterEl };
