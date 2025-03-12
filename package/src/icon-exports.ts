1ff519d893e640a7682b8409feed2f3b98518f16
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-12 15:59:50 +0530

    add linkedin icon

--- package/src/icon-exports.ts
+++ package/src/icon-exports.ts
@@ -1,6 +1,17 @@
 import CircleCheckIcon from './icons/CircleCheckIcon.astro';
 import CircleXIcon from './icons/CircleXIcon.astro';
+import FacebookIcon from './icons/FacebookIcon.astro';
+import GithubIcon from './icons/GithubIcon.astro';
 import InfoIcon from './icons/InfoIcon.astro';
+import LinkedInIcon from './icons/LinkedInIcon.astro';
 import TriangleAlertIcon from './icons/TriangleAlertIcon.astro';
 
-export { CircleCheckIcon, CircleXIcon, InfoIcon, TriangleAlertIcon };
+export {
+  CircleCheckIcon,
+  CircleXIcon,
+  FacebookIcon,
+  GithubIcon,
+  InfoIcon,
+  LinkedInIcon,
+  TriangleAlertIcon,
+};
