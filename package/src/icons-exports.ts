306e345ebb44e296ecaf1653aa967268d02c383f
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-12 22:13:20 +0530

    package: add mail icon

--- package/src/icons-exports.ts
+++ package/src/icons-exports.ts
@@ -10,6 +10,7 @@ import TwitterIcon from './components/icons/brand/TwitterIcon.astro';
 import YoutubeIcon from './components/icons/brand/YoutubeIcon.astro';
 import CircleXIcon from './components/icons/CircleXIcon.astro';
 import HeartIcon from './components/icons/HeartIcon.astro';
+import MailIcon from './components/icons/mail/MailIcon.astro';
 import CircleCheckIcon from './components/icons/notification/CircleCheckIcon.astro';
 import InfoIcon from './components/icons/notification/InfoIcon.astro';
 import TriangleAlertIcon from './components/icons/notification/TriangleAlertIcon.astro';
@@ -26,6 +27,7 @@ export {
   InfoIcon,
   InstagramIcon,
   LinkedInIcon,
+  MailIcon,
   SlackIcon,
   TriangleAlertIcon,
   TwitterIcon,
