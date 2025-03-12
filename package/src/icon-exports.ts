a5fddf773a2a549992f2503b6141434fa9d8362d
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-12 16:14:50 +0530

    add lucid icons (#4)
    * package(icon): add circle check icon
    
    * add triangle alert icon
    
    * export new icons
    
    * add dummy icon component
    
    * add circle x icon
    
    * add github icon
    
    * add facebook icon
    
    * add linkedin icon
    
    * add slack icon
    
    * add youtube icon
    
    * add figma icon
    
    * add instagram icon
    
    * add twitter icon
    
    * remove sample icon
    
    * add gitlab icon
    
    * add chrome icon
    
    * add icon example

--- package/src/icon-exports.ts
+++ package/src/icon-exports.ts
@@ -1,3 +1,31 @@
+import ChromeIcon from './icons/ChromeIcon.astro';
+import CircleCheckIcon from './icons/CircleCheckIcon.astro';
+import CircleXIcon from './icons/CircleXIcon.astro';
+import FacebookIcon from './icons/FacebookIcon.astro';
+import FigmaIcon from './icons/FigmaIcon.astro';
+import GithubIcon from './icons/GithubIcon.astro';
+import GitlabIcon from './icons/GitlabIcon.astro';
 import InfoIcon from './icons/InfoIcon.astro';
+import InstagramIcon from './icons/InstagramIcon.astro';
+import LinkedInIcon from './icons/LinkedInIcon.astro';
+import SlackIcon from './icons/SlackIcon.astro';
+import TriangleAlertIcon from './icons/TriangleAlertIcon.astro';
+import TwitterIcon from './icons/TwitterIcon.astro';
+import YoutubeIcon from './icons/YoutubeIcon.astro';
 
-export { InfoIcon };
+export {
+  ChromeIcon,
+  CircleCheckIcon,
+  CircleXIcon,
+  FacebookIcon,
+  FigmaIcon,
+  GithubIcon,
+  GitlabIcon,
+  InfoIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  SlackIcon,
+  TriangleAlertIcon,
+  TwitterIcon,
+  YoutubeIcon,
+};
