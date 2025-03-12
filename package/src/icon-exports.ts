cec01f68efbf8132845bc937e0cf561adf5be5ef
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-12 16:08:00 +0530

    add instagram icon

--- package/src/icon-exports.ts
+++ package/src/icon-exports.ts
@@ -1,8 +1,10 @@
 import CircleCheckIcon from './icons/CircleCheckIcon.astro';
 import CircleXIcon from './icons/CircleXIcon.astro';
 import FacebookIcon from './icons/FacebookIcon.astro';
+import FigmaIcon from './icons/FigmaIcon.astro';
 import GithubIcon from './icons/GithubIcon.astro';
 import InfoIcon from './icons/InfoIcon.astro';
+import InstagramIcon from './icons/InstagramIcon.astro';
 import LinkedInIcon from './icons/LinkedInIcon.astro';
 import SlackIcon from './icons/SlackIcon.astro';
 import TriangleAlertIcon from './icons/TriangleAlertIcon.astro';
@@ -12,8 +14,10 @@ export {
   CircleCheckIcon,
   CircleXIcon,
   FacebookIcon,
+  FigmaIcon,
   GithubIcon,
   InfoIcon,
+  InstagramIcon,
   LinkedInIcon,
   SlackIcon,
   TriangleAlertIcon,
