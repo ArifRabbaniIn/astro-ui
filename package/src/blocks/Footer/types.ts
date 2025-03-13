cda044f4e1319ce6bead65cafa5bc56c3427b8e7
Arif Rabbani <arif.93rabbani@gmail.com>
2025-03-14 02:16:56 +0530

    package: remove footer block

--- package/src/blocks/Footer/types.ts
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from 'astro/types';
-import type Footer1 from './Footer1.astro';
-
-export type FooterProps = Footer1;
-
-type Footer1 = {
-  variant: '1';
-} & ComponentProps<typeof Footer1>;
